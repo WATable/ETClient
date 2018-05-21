ef38211e292da3d31c951a866e4fd0bc9f9752f5
snake <snake@cosyjoy.com>
2018-05-21 20:23:41 +0800

    client

---
+++ Assets/Init.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+//using Model;
+using System.Net;
+
+public class Init : MonoBehaviour
+{
+    public InputField pname;
+    public InputField password;
+    public Button loginBut;
+    public Button enterMap;
+
+    GameObject uiLogin;
+    GameObject uiLobby;
+
+    private async void Start()
+    {
+
+    }
+}
